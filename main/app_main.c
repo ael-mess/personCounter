1ff774f5d19735ad6b793ba2561dcd02d164237d
Amine <amine.elmessaoudi@icloud.com>
2022-01-18 15:41:12 +0100

    Adding main function

---
+++ main/app_main.c
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2022 ael-mess
+ *
+ * @file    app_main.c
+ * @brief   Startup.
+ * @author  ael-mess
+ *
+ * @addtogroup MAIN
+ * @{
+ */
+
+#include "esp_err.h"
+
+void app_main(void) {}
+
+/** @} */
